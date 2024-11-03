5c317124d53138799cb39b0e9d09d8b0fc95b4a5
unknown <sheikhalii190@gmail.com>
2024-11-03 07:41:30 +0500

    artical

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Img from './components/img';
+import MyArtical from './components/MyArtical';
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Img/>
+      <MyArtical/>
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/components/Img.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Img = () => {
+  return (
+    <div className='image shadow'>
+      <img src="https://imageio.forbes.com/specials-images/imageserve/6707de51220878bf60e797a0/0x0.jpg?format=jpg&width=595" alt="" width={400} height={300}/>
+    </div>
+  );
+}
+
+export default Img;
